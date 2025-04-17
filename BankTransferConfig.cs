d665559d9cffae6931a8fca737ea155c5811c0ff
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-04-17 11:06:36 +0700

    aplikasi bank transfer dengan runtime configuration

---
+++ BankTransferConfig.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace modul8_103022300001
+{
+    class Transfer
+    {
+        public int threshold { get; set; }
+        public int low_fee { get; set; }
+        public int high_fee { get; set; }
+
+        public Transfer (int threshold, int low_fee, int high_fee)
+        {
+            this.threshold = threshold;
+            this.low_fee = low_fee;
+            this.high_fee = high_fee;
+        }
+    }
+
+    class Confirmation
+    {
+        public String en { get; set; }
+        public String id { get; set; }
+
+        public Confirmation (String en, String id)
+        {
+            this.en = en;
+            this.id = id;
+        }
+    }
+
+    internal class BankTransferConfig
+    {
+
+        public String lang {  get; set; }
+        public String[] methods { get; set; }
+        public Transfer transfer {  get; set; }
+        public  Confirmation confirmation { get; set; }
+
+        public BankTransferConfig() { }
+        public BankTransferConfig(string lang, string[] methods, Transfer transfer, Confirmation confirmation)
+        {
+            this.lang = lang;
+            this.methods = methods;
+            this.transfer = transfer;
+            this.confirmation = confirmation;
+        }
+
+    }
+
+    class UIConfig
+    {
+        public BankTransferConfig bankTransferConfig;
+        public const String filePath = @"bank_transfer_config.json";
+
+        private BankTransferConfig ReadConfigFile()
+        {
+            var json = File.ReadAllText(filePath);
+            bankTransferConfig = JsonSerializer.Deserialize<BankTransferConfig>(json);
+            return bankTransferConfig;
+        }
+
+        private void SetDefault ()
+        {
+            bankTransferConfig = new BankTransferConfig();
+            bankTransferConfig.transfer = new Transfer(25000000, 6500, 15000);
+            bankTransferConfig.confirmation = new Confirmation("yes", "ya");
+            bankTransferConfig.lang = "en";
+            bankTransferConfig.methods = ["RTO (real-time)", "SKN", "RTGS", "BI FAST"];
+        }
+
+        private void WriteNewConfigFile()
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+            String jsonString = JsonSerializer.Serialize(bankTransferConfig, options);
+            File.WriteAllText(filePath, jsonString);
+        }
+
+        public UIConfig()
+        {
+            try
+            {
+                ReadConfigFile();
+            }
+            catch (Exception)
+            {
+                SetDefault();
+                WriteNewConfigFile();
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,52 @@
-﻿internal class Program
+﻿using System.Security.Cryptography.X509Certificates;
+using modul8_103022300001;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("commit pertama!");
+        int banyakTransfer;
+        int biayaTf;
+
+        BankTransferConfig config = new BankTransferConfig();
+
+
+        if (config.lang == "en")
+        {
+
+            Console.WriteLine("Please insert the amount of money to transfer:");
+            banyakTransfer = Convert.ToInt32(Console.ReadLine());
+        }
+        else
+        {
+            Console.WriteLine("Masukkan jumlah uang yang akan di-transfer:");
+            banyakTransfer = Convert.ToInt32(Console.ReadLine());
+        }
+
+        if (banyakTransfer < config.transfer.threshold)
+        {
+            Console.WriteLine(config.transfer.low_fee);
+            biayaTf = config.transfer.low_fee;
+
+        } else
+        {
+            Console.WriteLine(config.transfer.high_fee);
+            biayaTf = config.transfer.high_fee;
+        }
+
+        if (config.lang == "en")
+        {
+            Console.WriteLine("Transfer fee = " + biayaTf);
+            Console.WriteLine("Total amount = " + (banyakTransfer + biayaTf));
+            Console.WriteLine("Select transfer method:");
+        }
+        else
+        {
+            Console.WriteLine("Biaya transfer = " + biayaTf);
+            Console.WriteLine("Total biaya = " + (banyakTransfer + biayaTf));
+            Console.WriteLine("Pilih metode transfer:");
+        }
+
+
     }
 }
