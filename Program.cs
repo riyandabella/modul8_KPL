ff20f980f954644282c6bf8948c99873aa8caa3a
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-04-17 09:51:30 +0700

    initiap state

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("commit pertama!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("commit pertama!");
+    }
+}
